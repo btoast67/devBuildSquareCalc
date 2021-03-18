6b537b3c8a49853f2326f55d53583e073b898126
Miranda <alejandromiranda@quickenloans.com>
2021-03-18 13:54:03 -0400

    Add project files.

---
+++ devBuildSquareCalc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace devBuildSquareCalc
+{
+	class Program
+	{
+		public int GetArea(int side)
+		{
+			int area = side * side;
+			return area;
+		}
+		public int GetPerimeter(int side)
+		{
+			int perimeter = side * 4;
+			return perimeter;
+		}
+
+		static void Main(string[] args)
+		{
+			Program pro = new Program();
+			Console.WriteLine("Welcome to our Square Calculator.");
+			Console.WriteLine("Tell me the length of a side: ");
+			String input = Console.ReadLine();
+			int side = int.Parse(input);
+			Console.Write("The Area is:");
+			Console.WriteLine(pro.GetArea(side));
+			Console.Write("The Perimeter is:");
+			Console.WriteLine(pro.GetPerimeter(side));
+		}
+	}
+}
